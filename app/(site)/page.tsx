337d5e1a999ca43e20d944fae4e7ceff38128e8f
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-06-16 02:24:00 +0530

    add header component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-green-500">
-      <h1>Main Content</h1>
+    <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
+      <Header>new</Header>
     </div>
   );
 }
